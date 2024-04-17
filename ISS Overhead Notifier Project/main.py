2cef1e5b1b09b8c51e8a0aaea95a53ed44ee1e63
Ish <sesay_ishmael@icloud.com>
2024-04-17 00:41:44 -0500

    updating iss overhead project without password include

--- ISS Overhead Notifier Project/main.py
+++ ISS Overhead Notifier Project/main.py
@@ -28,21 +28,19 @@ def is_night():
     time_now = datetime.now().hour
 
     parameters = {
-        "lat" : MY_LAT,
-        "lng" : MY_LONG,
-        "formatted" : 0
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0
     }
 
-    response = requests.get("https://api.sunrise-sunset.org/json", params = parameters)
+    response = response.get("https://api.sunrise-sunset.org/json",params = parameters)
     response.raise_for_status()
     data = response.json()
+    sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+    sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-    if sunset <= time_now <= sunrise:
+    if time_now >= sunset and time_now <= sunrise:
         return True
-
 
 while True:
     time.sleep(60)
@@ -51,19 +49,11 @@ while True:
         connection.starttls()
         connection.login(MY_EMAIL,MY_PASSWORD)
         connection.send_mail(
-
             from_addr = MY_EMAIL,
             to_addr = MY_EMAIL,
             msg = "Subject: ISS location notification\n\nLook Up Homie!"
 
         )
-
-
-
-
-
-
-
 
 
 
